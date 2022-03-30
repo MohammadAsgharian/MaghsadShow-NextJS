d684a344edb3b4590410d27fd4b41c423e8b3671
MohammadAsgharian <m.asgharian@gmail.com>
2022-03-30 12:09:57 +0430

    master

--- containers/SignIn.jsx
+++ containers/SignIn.jsx
@@ -3,24 +3,28 @@ import Link from "next/link";
 import { toast } from "react-toastify";
 import axios from "axios";
 import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const submitHandler = async (e) => {
     e.preventDefault();
+    alert("asdasd");
     const loginDto = {
       Email: email,
       Password: password,
     };
     try {
+      setLoading(true);
       const CONTAINER_URL = `${process.env.API_URL}/account/login`;
       const response = await axios.post(
         `${process.env.API_URL}/account/login`,
         loginDto
       );
-      console.log("response", response);
+      setLoading(false);
       const roles = response?.data?.roles;
       const userName = response?.data?.username;
       const token = response?.data?.token;
@@ -47,7 +51,14 @@ const SignIn = () => {
       />
       <label htmlFor="Password">رمزعبور</label>
       <input type="password" onChange={(e) => setPassword(e.target.value)} />
-      <Button variant="contained">ورود</Button>
+      <Button
+        variant="contained"
+        type="submit"
+        disabled={loading ? true : false}
+      >
+        {loading && <CircularProgress size={14} />}
+        {!loading && "Click Me"}
+      </Button>
     </form>
   );
 };
